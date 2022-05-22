88518091582393a7f917c0975676c56c1c8e7147
Khaled_AMARI <khaled.amari@novhadrim.com>
2022-05-22 22:35:59 +0100

    Implement Home section

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const controlsContainer = document.querySelector('[data-controls-container]');
-let controlBtns = document.querySelectorAll('[data-control]');
+const controlBtns = document.querySelectorAll('[data-control]');
+const allSections = document.querySelectorAll('[ data-section]');
 
 window.addEventListener('load', startInteractions());
 
@@ -10,4 +11,13 @@ function startInteractions() {
 function handleClick(e) {
   document.querySelector('.active-btn').classList.remove('active-btn');
   e.target.classList.add('active-btn');
+  allSections.forEach(section => {
+    if (e.target.dataset.control === section.id) {
+      section.classList.add('active');
+      section.classList.remove('hide');
+    } else {
+      section.classList.remove('active');
+      section.classList.add('hide');
+    };
+  });
 };
