d78b797006b600223d94b5b0557b992f02e71fab
Khaled_AMARI <khaled.amari@novhadrim.com>
2022-05-28 17:36:40 +0100

    Add button to switch between dark an light mode.

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const controlsContainer = document.querySelector('[data-controls-container]');
 const controlBtns = document.querySelectorAll('[data-control]');
 const allSections = document.querySelectorAll('[ data-section]');
+const switchThemBtn = document.querySelector('[data-switch-theme]');
 
 window.addEventListener('load', startInteractions());
 
 function startInteractions() {
-  controlBtns.forEach(btn => btn.addEventListener("click", handleClick));
+  controlBtns.forEach(btn => btn.addEventListener("click", handleClickControls));
+  switchThemBtn.addEventListener('click', handleClickSwitchTheme)
 };
 
-function handleClick(e) {
+function handleClickControls(e) {
   document.querySelector('.active-btn').classList.remove('active-btn');
   e.target.classList.add('active-btn');
   allSections.forEach(section => {
@@ -20,4 +22,9 @@ function handleClick(e) {
       section.classList.add('hide');
     };
   });
+};
+
+function handleClickSwitchTheme() {
+  let body = document.body;
+  body.classList.toggle('light-mode');
 };
